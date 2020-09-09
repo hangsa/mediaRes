6ec1f076622a9450491cd62ee56d67aaa573f0ac
sa-macbook <lihailong0510@163.com>
2020-09-09 22:40:53 +0800

    add transform to id_name

--- medias/R/medias.R
+++ medias/R/medias.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,45 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
+transTon <- function(field, id_name){#transform id to name
+  refiel <- str_replace_na(field)
+  ln = nrow(id_name)
+
+  for(i in 1:ln){
+    id = id_name[[1]][i]
+    ide = str_c('(?<=\\|)', id, '(?=\\|)')
+
+    refiel = str_replace_all(refiel, ide, id_name[[2]][i])
+  }
+
+  return(refiel)
+}
+
+
+wipeValid <- function(field, tags){
+  refiel <- str_replace_na(field)
+  tlen <- length(tags)
+
+  for(i in 1:tlen){
+    tg = str_c('\\|', tags[i], '\\|')
+    refiel = str_replace_all(refiel, tg, '|')
+  }
+
+  refiel = str_replace_all(refiel, '(?<![0-9])\\|(?![0-9])', '')
+
+  return(refiel)
+}
+
+wipeValid_f <- function(fields, tags, fgenr, tgenr, rgenr){##fields and tags all have attribute fstlvlname
+  #fgenr, genre of fields need to wipe off;
+  #tgenr, genre of tags to wipe fields
+  #rgenr, restrict genre
+  rg = tags[[rgenr]] %>% unique
+  fields[[fgenr]] = str_replace_na(fields[[fgenr]])
+
+  for(r in rg){
+    utg <- tags %>% filter(.data[[rgenr]] == r) %>% `[[`(tgenr)
+    fields <- fields %>% mutate(.data[[fgenr]] = ifelse(.data[[rgenr]] == r,
+                                                        wipeValid()))
+  }
 }
